f316c2a28a33bcbad74d6b533f3d2bb5dfa602fa
Jaume Duatis <duatis@gmail.com>
2016-07-21 00:03:52 +0200

    Use 'let' instead of clousure funtion on seeding loop

--- test/seed.js
+++ test/seed.js
@@ -3,29 +3,29 @@
  */
 var faker      = require('faker'),
     company    = new (require('../controllers/companyController.js')),
-    comment    = new (require('../controllers/commentController.js')),
-    i  = 20;
+    comment    = new (require('../controllers/commentController.js'));
 
 company.remove({}, (err,data) => {
     comment.remove({}, (err,data) => {
-        for (; i--;) {
+        for (let i  = 20; i--;) {
             var _company = {
                 name: faker.name.findName(),
                 description: faker.lorem.paragraph(),
                 email: faker.internet.email()
             };
 
-            ((j) => {
-                company.create(_company, (err, data)=> {
-                    var _comment1 = {text: faker.lorem.paragraph(), _company: data._id};
-                    var _comment2 = {text: faker.lorem.paragraph(), _company: data._id};
-                    comment.create(_comment1, (err, data) => {
-                        comment.create(_comment2, (err, data) => {
-                            if (j == 0)process.exit();
-                        });
+            company.create(_company, (err, data)=> {
+                var _comment1 = {text: faker.lorem.paragraph(), _company: data._id};
+                var _comment2 = {text: faker.lorem.paragraph(), _company: data._id};
+                comment.create(_comment1, (err, data) => {
+                    comment.create(_comment2, (err, data) => {
+                        if (i == 0){
+                            console.log("Done");
+                            process.exit();
+                        }
                     });
                 });
-            })(i);
+            });
         }
     });
 });
